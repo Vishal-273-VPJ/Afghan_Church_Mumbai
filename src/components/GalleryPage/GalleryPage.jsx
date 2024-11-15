3b7a53e0c35a827263f404087b0dfead44782e34
Ajay7616 <u.ajaykumar7616@gmail.com>
2024-11-15 19:26:02 +0530

    Updated

--- src/components/GalleryPage/GalleryPage.jsx
+++ src/components/GalleryPage/GalleryPage.jsx
@@ -2,32 +2,181 @@ import React from 'react';
 import './GalleryPage.css';
 
 const GalleryPage = () => {
-  const images = [
-    '4241.jpg', '4245.jpg', '4283.jpg', '4295.jpg', '4314.jpg',
-    '4349.jpg', '4389.jpg', '4419.jpg', '4481.jpg', '4499.jpg',
-    '4550.jpg', '4592.jpg', '4612.jpg', '4635.jpg', '4676.jpg',
-    '4682.jpg', '4724.jpg', '4757.jpg', '4806.jpg', '4819.jpg',
-    '4861.jpg', '5689.jpg', '5705.jpg', '5708.jpg', '5732.jpg',
-    '5745.jpg', '7983.jpg', '8137.jpg', '8250.jpg', '8293.jpg',
-    '8326.jpg', '8414.jpg', '8426.jpg', '8492.jpg'
-  ];
-
   return (
     <div className='galleryPage pb-3'>
       <h3 className="text-center pb-3">Gallery</h3>
       <div className="container">
         <div className="row g-5">
-          {images.map((image, index) => (
-            <div className="col-md-4" key={index}>
-              <div className="p-3 bg-light shadow-lg rounded"> {/* Bootstrap shadow-lg and rounded corners */}
-                <img
-                  src={`${image}`}
-                  alt={`Gallery item ${index + 1}`}
-                  className="img-fluid rounded" // Optional: rounded corners on images as well
-                />
-              </div>
+          <div className="col-md-4">
+            <div className="p-3 bg-light shadow-lg rounded">
+              <img src="4241.jpg" alt="Gallery item 1" className="img-fluid rounded" />
             </div>
-          ))}
+          </div>
+          <div className="col-md-4">
+            <div className="p-3 bg-light shadow-lg rounded">
+              <img src="4245.jpg" alt="Gallery item 2" className="img-fluid rounded" />
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="p-3 bg-light shadow-lg rounded">
+              <img src="4283.jpg" alt="Gallery item 3" className="img-fluid rounded" />
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="p-3 bg-light shadow-lg rounded">
+              <img src="4295.jpg" alt="Gallery item 4" className="img-fluid rounded" />
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="p-3 bg-light shadow-lg rounded">
+              <img src="4314.jpg" alt="Gallery item 5" className="img-fluid rounded" />
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="p-3 bg-light shadow-lg rounded">
+              <img src="4349.jpg" alt="Gallery item 6" className="img-fluid rounded" />
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="p-3 bg-light shadow-lg rounded">
+              <img src="4389.jpg" alt="Gallery item 7" className="img-fluid rounded" />
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="p-3 bg-light shadow-lg rounded">
+              <img src="4419.jpg" alt="Gallery item 8" className="img-fluid rounded" />
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="p-3 bg-light shadow-lg rounded">
+              <img src="4481.jpg" alt="Gallery item 9" className="img-fluid rounded" />
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="p-3 bg-light shadow-lg rounded">
+              <img src="4499.jpg" alt="Gallery item 10" className="img-fluid rounded" />
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="p-3 bg-light shadow-lg rounded">
+              <img src="4550.jpg" alt="Gallery item 11" className="img-fluid rounded" />
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="p-3 bg-light shadow-lg rounded">
+              <img src="4592.jpg" alt="Gallery item 12" className="img-fluid rounded" />
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="p-3 bg-light shadow-lg rounded">
+              <img src="4612.jpg" alt="Gallery item 13" className="img-fluid rounded" />
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="p-3 bg-light shadow-lg rounded">
+              <img src="4635.jpg" alt="Gallery item 14" className="img-fluid rounded" />
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="p-3 bg-light shadow-lg rounded">
+              <img src="4676.jpg" alt="Gallery item 15" className="img-fluid rounded" />
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="p-3 bg-light shadow-lg rounded">
+              <img src="4682.jpg" alt="Gallery item 16" className="img-fluid rounded" />
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="p-3 bg-light shadow-lg rounded">
+              <img src="4724.jpg" alt="Gallery item 17" className="img-fluid rounded" />
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="p-3 bg-light shadow-lg rounded">
+              <img src="4757.jpg" alt="Gallery item 18" className="img-fluid rounded" />
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="p-3 bg-light shadow-lg rounded">
+              <img src="4806.jpg" alt="Gallery item 19" className="img-fluid rounded" />
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="p-3 bg-light shadow-lg rounded">
+              <img src="4819.jpg" alt="Gallery item 20" className="img-fluid rounded" />
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="p-3 bg-light shadow-lg rounded">
+              <img src="4861.jpg" alt="Gallery item 21" className="img-fluid rounded" />
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="p-3 bg-light shadow-lg rounded">
+              <img src="5689.jpg" alt="Gallery item 22" className="img-fluid rounded" />
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="p-3 bg-light shadow-lg rounded">
+              <img src="5705.jpg" alt="Gallery item 23" className="img-fluid rounded" />
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="p-3 bg-light shadow-lg rounded">
+              <img src="5708.jpg" alt="Gallery item 24" className="img-fluid rounded" />
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="p-3 bg-light shadow-lg rounded">
+              <img src="5732.jpg" alt="Gallery item 25" className="img-fluid rounded" />
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="p-3 bg-light shadow-lg rounded">
+              <img src="5745.jpg" alt="Gallery item 26" className="img-fluid rounded" />
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="p-3 bg-light shadow-lg rounded">
+              <img src="7983.jpg" alt="Gallery item 27" className="img-fluid rounded" />
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="p-3 bg-light shadow-lg rounded">
+              <img src="8137.jpg" alt="Gallery item 28" className="img-fluid rounded" />
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="p-3 bg-light shadow-lg rounded">
+              <img src="8250.jpg" alt="Gallery item 29" className="img-fluid rounded" />
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="p-3 bg-light shadow-lg rounded">
+              <img src="8293.jpg" alt="Gallery item 30" className="img-fluid rounded" />
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="p-3 bg-light shadow-lg rounded">
+              <img src="8326.jpg" alt="Gallery item 31" className="img-fluid rounded" />
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="p-3 bg-light shadow-lg rounded">
+              <img src="8414.jpg" alt="Gallery item 32" className="img-fluid rounded" />
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="p-3 bg-light shadow-lg rounded">
+              <img src="8426.jpg" alt="Gallery item 33" className="img-fluid rounded" />
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="p-3 bg-light shadow-lg rounded">
+              <img src="8492.jpg" alt="Gallery item 34" className="img-fluid rounded" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
